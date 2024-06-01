9356dc7d09ce4a73c3a45f8e2846015478ee8030
Surajit <Surajit092@gm.com>
2024-06-02 00:38:18 +0530

    Claculator

---
+++ pthn.py
@@ -0,0 +1,13 @@
+ # Calculater
+x=eval(input("Enter 1st Number"))
+y=eval(input("Enter 2nd Number"))
+print("+ or - or * or/")
+op=input("Enter Operator")
+if op=="+":
+     print("Addition is= ",x+y)
+elif op=="-":
+     print("Subtraction is= ",x-y)
+elif op=="*":
+     print("Multiplication is= ",x*y)
+else :
+     print("Division is= ",x/y)
